95b32b271c0cd0337bcbb2e883a3d02b4b8b1ca2
JoseskVolpe <joseskvolpe@gmail.com>
2022-06-21 23:09:34 -0300

    Environment variables

--- src/main.c
+++ src/main.c
@@ -1,5 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+
+const char* app_name = "passwden";
+
+char *home, *config_dir;
+
 int main(int argc, char* argv[]){
 
-    return 0;
+    home = getenv("HOME");
+    config_dir = malloc(strlen(home)+strlen("/.config/")+strlen(app_name)+1);
+    strcpy(config_dir, home);
+    strcat(config_dir, "/.config/");
+    strcat(config_dir, app_name);
+    strcat(config_dir, "/");
 
+
+
+    free(config_dir);
+
+    return 0;
 }
