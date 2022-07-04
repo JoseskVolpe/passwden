1dd440a82b1d9e8f803ccbe7825e298da97665ac
JoseskVolpe <joseskvolpe@gmail.com>
2022-07-04 20:54:46 -0300

    Release 0.12.0

--- src/main.c
+++ src/main.c
@@ -23,7 +23,7 @@
 
 //#define REQUIRED_GPGME "1.12.0" //There's no method with minimum version spec for now
 
-#define VERSION "0.11.0"
+#define VERSION "0.12.0"
 
 #include <gpgme.h>
 #include <zconf.h>
